a76064d0137971c835b9f1187895c02ea6b5a64e
dfmGit <83708183+dfmGit@users.noreply.github.com>
2025-04-01 14:57:48 +0200

    Delete inventories directory

--- inventories/script_migrations/sleep_20s.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import json, time
-
-time.sleep(20)
-inventory = dict()
-print(json.dumps(inventory))
--- inventories/script_migrations/sleep_2s.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import json, time
-
-time.sleep(2)
-inventory = dict()
-print(json.dumps(inventory))
