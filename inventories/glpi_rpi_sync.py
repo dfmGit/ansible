003811af1cdf74fa7f5a12370f358c9b6e5bc18e
dfmGit <83708183+dfmGit@users.noreply.github.com>
2026-05-05 11:19:15 +0200

    Update glpi_rpi_sync.py

--- inventories/glpi_rpi_sync.py
+++ inventories/glpi_rpi_sync.py
@@ -22,7 +22,7 @@ ONLY_PREFIX = os.environ.get("GLPI_ONLY_PREFIX", "rpi-")
 
 
 def glpi_url(path):
-    return GLPI_URL +  path
+    return GLPI_URL.rstrip("/") + "/apirest.php" + path
 
 
 def init_session():
